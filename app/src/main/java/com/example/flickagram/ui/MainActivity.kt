e488683d084b3990df17e0c4024e182e09cbba47
lalitdehraj <lalitrock97@gmail.com>
2022-03-28 18:24:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/flickagram/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flickagram.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flickagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
