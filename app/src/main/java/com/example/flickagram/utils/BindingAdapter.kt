8aca8d130a335b816f743846562c11675c9b8658
lalitdehraj <lalitrock97@gmail.com>
2022-03-30 19:42:30 +0530

    reCommit

--- app/src/main/java/com/example/flickagram/utils/BindingAdapter.kt
+++ app/src/main/java/com/example/flickagram/utils/BindingAdapter.kt
@@ -3,8 +3,9 @@ package com.example.flickagram.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.flickagram.R
 
 @BindingAdapter("loadImage")
 fun loadImageInImageView(imageView: ImageView, url : String?) {
-    Glide.with(imageView).load(url).into(imageView)
+    Glide.with(imageView).load(url).placeholder(R.drawable.img).into(imageView)
 }
